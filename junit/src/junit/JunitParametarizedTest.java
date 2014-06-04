ce54211381265a72c1d94bc0d72b21ed55312341
tamil <tamilselvan.guru@gmail.com>
2014-06-04 12:37:25 -0700

    junit parameterized

---
+++ junit/src/junit/JunitParametarizedTest.java
@@ -0,0 +1,34 @@
+package junit;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class JunitParametarizedTest {
+
+	private int number;
+	private String name;
+
+	public JunitParametarizedTest(int number, String name) {
+		this.number = number;
+		this.name = name;
+	}
+
+	@Parameters
+	public static Collection data() {
+		return Arrays.asList(new Object[][] { { 1, "Tamil" }, { 2, "Pranith" },
+				{ 3, "Sangeetha" } });
+	}
+
+	@Test
+	public void myTest() {
+		System.out.println("No: " + number);
+		System.out.println("Name: " + name);
+	}
+
+}
