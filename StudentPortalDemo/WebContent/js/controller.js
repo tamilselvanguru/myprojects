ff5165bcb8d33d7202063636b12e576025d97451
pranith <pranith@pranith-pc>
2016-01-03 11:04:25 -0600

    Added profile info of students

--- StudentPortalDemo/WebContent/js/controller.js
+++ StudentPortalDemo/WebContent/js/controller.js
@@ -1,4 +1,63 @@
-var helloApp = angular.module("helloApp", []);
-helloApp.controller("CompanyCtrl", function($scope) {
-	$scope.companies = [ 'hexa', 'tcs', 'cts', 'wipro', 'satyam', 'hcl' ];
+var StudentApp = angular.module("StudentApp", []);
+StudentApp.controller("StudentCtrl", function($scope) {
+	$scope.IsMyLabelVisible=true;
+	$scope.IsVisible = false;
+	$scope.students = [ {
+		'name' : 'Sangeetha',
+		'deparment' : 'MCA',
+		'idno' : '11085',
+		'phno' : '5124351233',
+		'address' : [ {
+			'streetAddress' : 'Manal Thottam',
+			'city' : 'Sathy',
+			'state' : 'TN',
+			'zipcode' : '638452'
+		} ]
+	},
+
+	{
+		'name' : 'Tamil',
+		'deparment' : 'ECE',
+		'idno' : '11067',
+		'phno' : '6127351543',
+		'address' : [ {
+			'streetAddress' : 'KG Thottam',
+			'city' : 'Gobi',
+			'state' : 'TN',
+			'zipcode' : '453678'
+		} ]
+	}, {
+		'name' : 'Pranu',
+		'deparment' : 'MBBS',
+		'idno' : '11034',
+		'phno' : '2121121256',
+		'address' : [ {
+			'streetAddress' : 'El camino',
+			'city' : 'Mountain view',
+			'state' : 'US',
+			'zipcode' : '78759'
+		} ]
+	}, {
+		'name' : 'Lalith',
+		'deparment' : 'MBBS',
+		'idno' : '11012',
+		'phno' : '1112435333',
+		'address' : [ {
+			'streetAddress' : 'Kom pallam',
+			'city' : 'Sathy',
+			'state' : 'TN',
+			'zipcode' : '567487'
+		} ]
+
+	} ];
+	$scope.student =$scope.students[0];
+	$scope.selectedStudent = function(value) {
+		$scope.IsVisible = false;
+		$scope.IsMyLabelVisible=true;
+		$scope.student = value;
+	};
+	$scope.show = function() {
+		$scope.IsMyLabelVisible=false;
+		$scope.IsVisible = true;
+	};
 });
--- StudentPortalDemo/WebContent/js/studentPortal.js
+++ StudentPortalDemo/WebContent/js/studentPortal.js
@@ -2,4 +2,6 @@ $(document).ready(function() {
 	$("#tabs_div").tabs();
 	$("#tabs_div").tabs("select", 0);
 	var myLayout = $('#container').layout();
+	
+	
 });
